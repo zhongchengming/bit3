071be6f67393d2fd9e166e923d8162196a3a5178
zhongchengming <501655944@qq.com>
2020-06-29 07:46:37 +0800

    ces

---
+++ src/main/java/com/ylzInfo/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.ylzInfo.controller;
+
+import com.ylzInfo.util.Result;
+import org.apache.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class UserController {
+//    @RequestBody
+//    @RequestMapping("/register")
+//    public Result register(HttpServletRequest request){
+//
+//
+//    }
+}
